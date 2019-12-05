1a71fec114663a73dc127c7e1b6dc473b041311a
xiaobo <linxiaobo110@gmail.com>
2019-12-05 16:47:01 +0800

    add camdown.py

--- CamDown.py
+++ CamDown.py
@@ -117,7 +117,7 @@ class CamDown(object):
 
 if __name__ == '__main__':
     " used for testing this module"
-    testFlag = 1
+    testFlag = 2
 
     if testFlag == 1:
         cam1 = CamDown()
@@ -125,9 +125,78 @@ if __name__ == '__main__':
         cam1.load_ground_img()
         print('Load img completed!')
         pos_0 = np.array([4251.97508977, -5843.00458236,   227.35483937])
-        att_0 = np.array([-0.13647458, -0.1990263 ,  0.27836947])
+        att_0 = np.array([-0.13647458, -0.1990263,  0.27836947])
         img1 = cam1.get_img_by_state(pos_0, att_0)
         import matplotlib.pyplot as plt
         plt.imshow(cam1.pixCamImg / 255)
         cv2.imwrite('Data/test.jpg', img1)
+
+    elif testFlag == 2:
+        import matplotlib.pyplot as plt
+        import matplotlib as mpl
+        from QuadrotorFlyModel import QuadModel, QuadSimOpt, QuadParas, StructureType, SimInitType
+        import MemoryStore
+        D2R = np.pi / 180
+
+        print("PID  controller test: ")
+        uavPara = QuadParas(structure_type=StructureType.quad_x)
+        simPara = QuadSimOpt(init_mode=SimInitType.fixed, enable_sensor_sys=False,
+                             init_att=np.array([10., -10., 30]), init_pos=np.array([5, -5, 0]))
+        quad1 = QuadModel(uavPara, simPara)
+        record = MemoryStore.DataRecord()
+        record.clear()
+        step_cnt = 0
+
+        # init the camera
+        cam1 = CamDown()
+        cam1.load_ground_img()
+        print('Load img completed!')
+        fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+        out1 = cv2.VideoWriter('Data/img/test.avi', fourcc, 1 / quad1.uavPara.ts, (cam1.imgVertical, cam1.imgHorizon))
+        for i in range(1000):
+            ref = np.array([0., 0., 1., 0.])
+            stateTemp = quad1.observe()
+            # get image
+            pos_0 = quad1.position * 1000
+            att_0 = quad1.attitude
+            img1 = cam1.get_img_by_state(pos_0, att_0)
+            # file_name = 'Data/img/test_' + str(i) + '.jpg'
+            # cv2.imwrite(file_name, img1)
+            out1.write(img1)
+
+            action2, oil = quad1.get_controller_pid(stateTemp, ref)
+            print('action: ', action2)
+            action2 = np.clip(action2, 0.1, 0.9)
+            quad1.step(action2)
+            record.buffer_append((stateTemp, action2))
+            step_cnt = step_cnt + 1
+        record.episode_append()
+        out1.release()
+
+        print('Quadrotor structure type', quad1.uavPara.structureType)
+        # quad1.reset_states()
+        print('Quadrotor get reward:', quad1.get_reward())
+        data = record.get_episode_buffer()
+        bs = data[0]
+        ba = data[1]
+        t = range(0, record.count)
+        # mpl.style.use('seaborn')
+        fig1 = plt.figure(1)
+        plt.clf()
+        plt.subplot(3, 1, 1)
+        plt.plot(t, bs[t, 6] / D2R, label='roll')
+        plt.plot(t, bs[t, 7] / D2R, label='pitch')
+        plt.plot(t, bs[t, 8] / D2R, label='yaw')
+        plt.ylabel('Attitude $(\circ)$', fontsize=15)
+        plt.legend(fontsize=15, bbox_to_anchor=(1, 1.05))
+        plt.subplot(3, 1, 2)
+        plt.plot(t, bs[t, 0], label='x')
+        plt.plot(t, bs[t, 1], label='y')
+        plt.ylabel('Position (m)', fontsize=15)
+        plt.legend(fontsize=15, bbox_to_anchor=(1, 1.05))
+        plt.subplot(3, 1, 3)
+        plt.plot(t, bs[t, 2], label='z')
+        plt.ylabel('Altitude (m)', fontsize=15)
+        plt.legend(fontsize=15, bbox_to_anchor=(1, 1.05))
+        plt.show()
 
